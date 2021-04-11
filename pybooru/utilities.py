b86e6674c7f7fd54ecd64547c2bef006f73874ed
Lieuwe Leene <lieuwe.leene@gmail.com>
2021-04-11 16:37:15 +0200

    WIP: Update on utility scripts cleaning up sync procedures

--- pybooru/utilities.py
+++ pybooru/utilities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from __future__ import unicode_literals
+from pybooru import Moebooru
 from pybooru import Danbooru
 from pybooru import Gelbooru
 from pybooru import E621
@@ -9,17 +10,17 @@ import sys
 import os
 import time
 
-
 def progressHandle(item_str, index_int, set_size_int):
   logging.info('Starting:' + item_str + ' ' + str(index_int) + '/' + str(set_size_int))
   while os.path.exists('./pause'):
     time.sleep(2)
 
-
 def mapResponse(items, map_type):
   # Define mappings,
   map_keys = { 'gelbooru_posts':{'hash':'md5', 'tags':'tag_string'},
     'gelbooru_tags':{'count':'post_count'},
+    'yandere_posts':{'tags':'tag_string'},
+    'yandere_tags':{'count':'post_count','type':'catagory'},
     'rule34_posts':{'tags':'tag_string'},
     'rule34_tags':{'count':'post_count'},
     'e621_posts':{},
@@ -60,7 +61,7 @@ def mapResponse(items, map_type):
 def get_tag_list(target_client, target_tag):
   try:
     tag_query = target_client.tag_list(name=target_tag)
-    if target_client.site_name != 'danbooru' and len(tag_query) > 0:
+    if len(tag_query) > 0 and target_client.site_name != 'danbooru' and target_client.site_name:
       tag_query = mapResponse(tag_query, target_client.site_name + '_tags')
   except Exception as Message:
     logging.warning('ETag:' + target_tag)
@@ -69,10 +70,10 @@ def get_tag_list(target_client, target_tag):
   return tag_query
 
 
-def get_post_list(target_client, target_tag, page_index):
+def get_post_list(target_client, target_tag, page_index, limit = 100):
   try:
-    post_query = target_client.post_list(tags=target_tag, page=page_index)
-    if target_client.site_name != 'danbooru' and len(post_query) > 0:
+    post_query = target_client.post_list(tags=target_tag, page=page_index, limit=limit)
+    if len(post_query) > 0 and target_client.site_name != 'danbooru' and target_client.site_name:
       post_query = mapResponse(post_query, target_client.site_name + '_posts')
   except Exception as Message:
     logging.warning('ETag:' + target_tag)
@@ -95,7 +96,7 @@ def retrieveMD5Set(target_client, artist_tag):
     posts = get_post_list(target_client, artist_tag, page_index)
     if len(posts) <= 0:
         break
-    md5_list += [sub['md5'] for sub in posts]
+    md5_list += ['md5' in sub and sub['md5'] for sub in posts]
     tag_count -= len(posts)
     page_index += 1
   return(set(md5_list))
@@ -137,12 +138,6 @@ def resolveNewTags(target_client, tag_string_list):
     if len(get_tag_list(target_client, tag)) <= 0:
       new_tags.update(tag)
 
-
-def updateNewTags(source_client, local_client, post_list):
-  tag_string_list = [sub['tag_string'] for sub in post_list]
-  resolveNewTags(target_client, tag_string_list)
-
-
 def commitPostList(target_client, post_list, md5_set, artist_tag):
   for post in post_list:
     # Skip any exising post
@@ -154,47 +149,29 @@ def commitPostList(target_client, post_list, md5_set, artist_tag):
       uploadFile(target_client, post, file_path)
 
 
-def fetchArtistPosts(target_client, artist_tag):
+def fetchArtistPosts(target_client, artist_tag, stop_on_subset = set()):
   artist_query = get_tag_list(target_client, artist_tag)
-  if len(artist_query) <= 0 or int(artist_query[0]['post_count']) <= 0:
+  if len(artist_query) <= 0 or int(artist_query[0]['post_count']) <= 0  or int(artist_query[0]['category']) != 1:
+    logging.warning('ETag:' + artist_tag + ' is not a valid artist tag')
     return []
   tag_count = int(artist_query[0]['post_count'])
   post_list = []
   page_index = 1
-  while tag_count > 0:
-    posts = get_post_list(target_client, artist_tag, page_index)
-    if len(posts) <= 0:
-      break
+  posts = get_post_list(target_client, artist_tag, page_index, 20)
+  md5_new = set(['md5' in sub and sub['md5'] for sub in posts])
+  while tag_count > 0 and not md5_new.issubset(stop_on_subset) and len(posts) > 0:
     post_list += posts
     tag_count -= len(posts)
     page_index += 1
-  return post_list
-
-
-def shallowFetchPosts(target_client, md5_set, artist_tag):
-  artist_query = get_tag_list(target_client, artist_tag)
-  if len(artist_query) <= 0 or int(artist_query[0]['post_count'] <= 0):
-    return []
-  tag_count = int(artist_query[0]['post_count'])
-  post_list = []
-  page_index = 1
-  md5_new = set(0)  # Always Pull once
-  while not md5_new.issubset(md5_set) and tag_count > 0:
-    posts = get_post_list(target_client, artist_tag, page_index)
-    if len(posts) <= 0:
-      break
-    md5_new = [sub['md5'] for sub in posts]
-    post_list += posts
-    tag_count -= len(posts)
-    page_index += 1
+    posts = get_post_list(target_client, artist_tag, page_index, 20)
+    md5_new = set(['md5' in sub and sub['md5'] for sub in posts])
   return post_list
 
 
 def getSourceCatagory(target_client, category, threshold=0):
   tag_name_list = []
   request_size = 100
-  map_category = {'general':'0', 'artist':'1', 'copyright':'3', 'character':'4', 'meta':'5'}
-  category_str = map_category[category]
+  category_str = target_client.map_category[category]
   # Fetch close to all artists on Source Booru
   for page_id in range(999):
     logging.info('Completed: ' + str(page_id) + '/999')
@@ -205,6 +182,25 @@ def getSourceCatagory(target_client, category, threshold=0):
       break
     tag_name_list += [sub['name'] for sub in tag_list if sub['post_count'] > threshold]
     if( len(tag_list) < request_size ):
+      break
+  return set(tag_name_list)
+
+
+def getMatchingTags(target_client, category, pattern, threshold=0):
+  tag_name_list = []
+  request_size = 100
+  # category_str = target_client.map_category[category]
+  # Fetch close to all artists on Source Booru
+  for page_id in range(999):
+    # logging.info('Completed: ' + str(page_id) + '/999')
+    try: # TODO consider case statment here using category_str
+      query_list = target_client.artist_list(any_name_matches=pattern, extra_params={'limit':request_size, 'page':page_id})
+    except Exception as Message:
+      logging.warning(Message)
+      break
+    for entry in query_list:
+      tag_name_list += [sub['name'] for sub in target_client.tag_list(name=entry['name']) if sub['post_count'] > threshold]
+    if( len(query_list) < request_size ):
       break
   return set(tag_name_list)
 
@@ -220,28 +216,19 @@ def saveSourceCatagory(target_client, category, file_name):
 def pullCategorySet(source_client):
   logging.basicConfig(filename=('output'+str(os.getpid())), level=logging.INFO, format='')
   for category in ['general', 'artist', 'copyright', 'character', 'meta']:
-    file_name = 'Lists/source_'+category+'_list.'+target_client.site_name
+    file_name = 'Lists/source_'+category+'_list.'+source_client.site_name
     saveSourceCatagory(source_client, category, file_name)
 
 
-def pullTagAliases(source_client, local_client):
-  logging.basicConfig(filename=('output'+str(os.getpid())), level=logging.INFO, format='')
-  alias_list = []
-  for page_id in range(999):
-    logging.info('Completed: ' + str(page_id) + '/999')
-    alias_list = source_client.tag_list(hide_empty='yes', order='count',category='0',
-      extra_params={'limit':100,'page':page_id})
-
-
 def syncLocalCategory(local_client, category_file, category):
-  map_category = {'general':'0', 'artist':'1', 'copyright':'3', 'character':'4', 'meta':'5'}
-  category_str = map_category[category]
+  category_str = local_client.map_category[category]
   item_list = []
   with open(category_file, 'r') as input_file:
     item_list = input_file.read().splitlines()
   source_set = set(item_list)
   # Start checking for tag discreptancies
   for page_id in range(999):
+    progressHandle(category, page_id, 999)
     tag_list = local_client.tag_list(hide_empty='yes', order='count', category='0',
       extra_params={'limit':100, 'page':page_id})
     local_name_list = [sub['name'] for sub in tag_list]
@@ -257,41 +244,107 @@ def syncLocalCategory(local_client, category_file, category):
           logging.warning(Message)
         finally:
           time.sleep(local_count_list[index]/10+0.2)
-    progressHandle(category, page_id, 999)
 
 
-def deepArtistRefresh(source_client, local_client, progress_file):
-  with open(progress_file, 'r') as file:
-    completed = set(file.read().splitlines())
-    artist_tags = getSourceCatagory(local_client, 'artist', threshold=10) ^ completed
-    set_size = len(artist_tags)
-    for index, artist_tag in enumerate(artist_tags):
-      if len(artist_tag) < 3:  # 2 character names at not valid
-        continue
+def artistRefresh(source_client, local_client, progress_file = None, fullScan= False):
+  artist_tags = getSourceCatagory(local_client, 'artist', threshold=10)
+  if progress_file:
+    with open(progress_file, 'r') as file:
+      completed = set(file.read().splitlines())
+      artist_tags = artist_tags ^ completed
+  set_size = len(artist_tags)
+  for index, artist_tag in enumerate(artist_tags):
+    progressHandle(artist_tag, index, set_size)
+    # or artist_tag == 'banned_artist':  # 2 character names at not valid
+    if len(artist_tag) < 3:
+      continue
+    md5_set = retrieveMD5Set(local_client, artist_tag)
+    if fullScan:
       post_list = fetchArtistPosts(source_client, artist_tag)
-      if len(post_list) > 0:
-        md5_set = retrieveMD5Set(local_client, artist_tag)
-        commitPostList(local_client, post_list, md5_set, artist_tag)
-      progressHandle(artist_tag, index, set_size)
+    else:
+      post_list = fetchArtistPosts(source_client, artist_tag, stop_on_subset=md5_set)
+    if len(post_list) > 0:
+      commitPostList(local_client, post_list, md5_set, artist_tag)
 
 
 def pullNewArtist(source_client, local_client, input_file, progress_file = None, force = False):
   with open(input_file, 'r') as file:
     artist_tags = set(file.read().splitlines())
-    with open(progress_file, 'r') as progress:
-      completed = set(progress.read().splitlines())
-      artist_tags = artist_tags ^ completed
-    if force == False:
-      artist_tags = new - getSourceCatagory(local_client, 'artist', threshold=10)
+    if progress_file:
+      with open(progress_file, 'r') as progress:
+        completed = set(progress.read().splitlines())
+        artist_tags = artist_tags ^ completed
+    if force != True:
+      artist_tags = artist_tags - getSourceCatagory(local_client, 'artist', threshold=10)
     set_size = len(artist_tags)
     for index, artist_tag in enumerate(artist_tags):
+      progressHandle(artist_tag, index, set_size)
       if len(artist_tag) < 3:
         continue
       post_list = fetchArtistPosts(source_client, artist_tag)
       if len(post_list) > 0:
         md5_set = retrieveMD5Set(local_client, artist_tag)
         commitPostList(local_client, post_list, md5_set, artist_tag)
-      progressHandle(artist_tag, index, set_size)
+
+
+def pullTagSet(source_client, local_client):
+  for line in sys.stdin:
+    # commitArtist(local_client, source_client, line)
+    # commitArtistTag(local_client, source_client, line) is broken
+    progressHandle(line, 0, 0)
+
+
+def initArtistEntries(source_client, local_client):
+  artist_tags = getSourceCatagory(local_client, 'artist', threshold=10)
+  set_size = len(artist_tags)
+  for index, artist_tag in enumerate(artist_tags):
+    progressHandle(artist_tag, index, set_size)
+    try:
+      local_entry = local_client.artist_list(query=artist_tag)
+    except Exception as Message:
+      logging.warning('ETag:' + artist_tag)
+      logging.warning(Message)
+    if len(local_entry) <= 0:
+      try:
+        local_client.artist_create(name=artist_tag)
+      except Exception as Message:
+        logging.warning('ETag:' + artist_tag)
+        logging.warning(Message)
+
+
+def pullArtistEntries(source_client, local_client):
+  # TODO actually update missing info
+  artist_tags = getSourceCatagory(local_client, 'artist', threshold=10)
+  set_size = len(artist_tags)
+  for index, artist_tag in enumerate(artist_tags):
+    progressHandle(artist_tag, index, set_size)
+    try:
+      source_entry = source_client.artist_list(query=artist_tag)
+      local_entry = local_client.artist_list(query=artist_tag)
+    except Exception as Message:
+      logging.warning('ETag:' + artist_tag)
+      logging.warning(Message)
+    if len(source_entry) > 0 and len(local_entry) <= 0:
+      try:
+        local_client.artist_create(name=source_entry[0]['name'], other_names=' '.join(source_entry[0]['other_names']), group_name=source_entry[0]['group_name'])
+      except Exception as Message:
+        logging.warning('ETag:' + artist_tag)
+        logging.warning(Message)
+
+
+def addToArtistNames( local_client, artist_tag, newName ):
+  try:
+    entry = local_client.artist_list(query=artist_tag)
+  except Exception as Message:
+    logging.warning('ETag:' + artist_tag)
+    logging.warning(Message)
+  if len(entry) > 0 and newName not in entry[0]['other_names']:
+    entry[0]['other_names'].append( newName )
+    try:
+      local_client.artist_update(entry[0]['id'], other_names=' '.join(entry[0]['other_names']))
+    except Exception as Message:
+      logging.warning('ETag:' + artist_tag)
+      logging.warning(Message)
 
 
 def syncAllCategories(local_client):
